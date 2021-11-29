ca78482c6103a020409d69c768b6abf126b281ea
LURZU <82831225+LURZU@users.noreply.github.com>
2021-11-29 17:18:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,13 +10,6 @@ window.addEventListener("load", function(event) {
 
 //API Pour carte de pollution de l'air
 
-var OSM_URL = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var OSM_ATTRIB = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a>contributors';
-var osmLayer= L.tileLayer(OSM_URL,{attribution: OSM_ATTRIB}); 
-var WAQI_URL = "https://tiles.waqi.info/tiles/usepa-aqi/{z}/{x}/{y}.png?token=_TOKEN_ID_";
-var WAQI_ATTR = 'Air Quality Tiles &copy; <a href="http://waqi.info">waqi.info</a>';
-var waqiLayer = L.tileLayer(WAQI_URL, {attribution: WAQI_ATTR}); 
-//API Pour carte de pollution de l'air
 
 var OSM_URL = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var OSM_ATTRIB = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a>contributors';
