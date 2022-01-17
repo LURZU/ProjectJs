63c60f3967def2dc74fd1ef65af69814b525f316
LURZU <82831225+LURZU@users.noreply.github.com>
2022-01-17 09:42:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,97 @@
 
 
 
-  //permet d'activer la map (bouton)
-  var map = L.map('map').setView([51.505, -0.09], 13);
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    maxZoom: 19,
+  var map = L.map('map').setView([51.505, -0.09], 3);
+var test = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  maxZoom: 10,
+  minZoom : 1,
+  zoomControl : false
 }).addTo(map);
+map.removeControl(map.zoomControl)
+
+
+
+
+
+function activemap(){
+    //selectionne et créer une div à remplacer avec la div map
+    let RemoveMap = document.getElementById('map')
+    let create = document.createElement("div");
+    create.id = "map"
+    $('#EnableMap').change(function() {
+        if(this.checked){
+        //Recréer la variable map pour l'appel de leaflet 
+            map = L.map('map').setView([51.505, -0.09], 3);
+            test = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            attribution:
+                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            maxZoom: 10,
+            minZoom : 1,
+            zoomControl : false
+            }).addTo(map);
+            map.removeControl(map.zoomControl)  
+    } else  {
+                RemoveMap.innerHTML =""
+                // Supprime la div de la map 
+                RemoveMap.replaceWith(create)
+    }
+});
+
+}
+
+////////////////////////////////////////////////////fonction pour changer de layer sur la map/////////////////////////////////////////////////////
+
+
+function layerchange(){
+ //pollution de l'air
+ if($('input[name=PollutionAir]').is(':checked') ){
+   
+    getAPI('https://api.waqi.info/map/bounds/?token=759f43417454ac9ceb83d8f0ff749abdf3a652e9&latlng=89.99994526655283,589.2187500000001,-89.9947414962234,-1431.5625', 'PollutionAir');
+   
+} else {
+    localStorage.removeItem('PollutionAir')
+    map.removeLayer(markersClusterAirPolution);
+}
+
+//pollution plastique 
+if( $('input[name=PollutionPlastique]').is(':checked') ){
+    pollutionMarine();
+    
+} else {
+    map.removeLayer(marker);
+}
+}
+
+
+
+
+
+
+
+
+
+///////////////////////////////////Initialisation des variables ////////////////////////////////////////
+
+//pollutionmarine
+var marker_pollutionMarine;
+var markersClusterMarine;
+var popup;
+var marker;
+//pollution air
+var markers_polutionAir = {};
+//group de marker pour la polution de l'air
+var markersClusterAirPolution = L.markerClusterGroup();
+//boundarys of the map x1, y1, x2, y2
+//var boundMap = [map.getBounds()._northEast.lat, map.getBounds()._northEast.lng, map.getBounds()._southWest.lat, map.getBounds()._southWest.lng];
+
+//89.99994526655283 589.2187500000001 -89.9947414962234 -1431.5625 world bound
+
+//Functions ////////////////////////////////////////////////////////
+
+map.on("moveend", function(){
+});
 
 
 
